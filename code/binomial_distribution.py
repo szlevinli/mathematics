de6622e624578b1a9e3fc4c3bfce6d19cc838319
levin li <3259963@qq.com>
2019-02-01 12:58:41 +0800

    采用np.lanspace使图中曲线更加顺滑

--- code/binomial_distribution.py
+++ code/binomial_distribution.py
@@ -36,14 +36,14 @@ def main():
     n = 10   # 试验次数
     p = 1/6  # 成功概率。以骰子为例，得到任一一个指定面比如4的概率
 
-    x = np.arange(1, n+1)
-    y = np.zeros(n)
+    # 使用 linspace 替代 arange 的目的是让画出的图更加顺滑
+    # x = np.arange(1, n+1) # 4在10次试验中出现的次数(从1到10)
+    x = np.linspace(1, n+1, 100)
 
     # 使用 `numpy` 提供的 `frompyfunc` 函数创建一个自定义 `Universal Function` 简称 `ufunc`
     # `ufunc`的功能是自动迭代 `ndarray` 中的元素应用于指定的函数，从而避免手工编写迭代代码
     test_ufunc = np.frompyfunc(bd_pmf, 3, 1)
     y = test_ufunc(n, p, x)
-    print(y)
 
     plt.plot(x, y)
     plt.show()
